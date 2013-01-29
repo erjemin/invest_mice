c005588350e525cf0bc402558765b51e43c073e8
erjemin <erjemin@rambler.ru>
2013-01-29 22:00:57 +0400

    Первый хелло-ворлд

--- www/views.py
+++ www/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+# Включили поддержку UTF-8 в Python. Без этого даже комментарии на русском языке нельзя писать.
+
+from django.http import HttpResponse, Http404
+import random
+import datetime
+import time
+
+
+def hello ( request ) :
+    return HttpResponse ( "Hello world! Привет питон!")
